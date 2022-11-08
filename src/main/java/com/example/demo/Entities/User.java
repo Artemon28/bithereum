9a3e2f7f4c0d1f2c4becb28ac7ed8d7c59c9281e
Artemiy <los28.2001@mail.ru>
2022-11-08 19:23:40 +0300

    architecture

---
+++ src/main/java/com/example/demo/Entities/User.java
@@ -0,0 +1,9 @@
+package com.example.demo.Entities;
+
+public class User {
+    private int id;
+    private String login;
+    private byte[] password;
+    private String nickname;
+    private String bitcoinToken;
+}
